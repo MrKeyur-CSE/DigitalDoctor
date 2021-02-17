25e809be87d3bd48dbdb6150128b12ee2fe046ac
Keyur Panchal <keyur.developer.cse@gmail.com>
2021-02-17 11:02:25 +0530

    17-02-2021_update

--- app/src/main/java/com/example/digitaldoctor/MainActivity.java
+++ app/src/main/java/com/example/digitaldoctor/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.digitaldoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.lang.Object;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,30 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // hide the title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.registration_main);
+        setContentView(R.layout.registration_doc);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.frombutton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                DialogFragment timePicker2 = new TimePickerFragment();
+                timePicker2.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+    }
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView textView = (TextView) findViewById(R.id.totimeadd);
+        textView.setText(hourOfDay + " : " + minute);
     }
 }
+
