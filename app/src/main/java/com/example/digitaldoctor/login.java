e7b72b096f391906cebdbe53cc001122579882dd
Tapan Panchal <tapan007panchal@gmail.com>
2021-02-18 12:33:24 +0530

    firebase authentication sdk-1

--- app/src/main/java/com/example/digitaldoctor/login.java
+++ app/src/main/java/com/example/digitaldoctor/login.java
@@ -1,14 +1,83 @@
 package com.example.digitaldoctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+
+    EditText mEmail, mPassword;
+    Button mLogin;
+    TextView mRegister;
+    FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        mEmail = findViewById(R.id.loginid);
+        mPassword = findViewById(R.id.login_password);
+        mLogin = findViewById(R.id.loginbutton);
+        mRegister = findViewById(R.id.Register);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if(pass.length()<6){
+                    mPassword.setError("Password must be >= 6 characters");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(login.this, "Logged In",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),after_login.class));
+                        }else{
+                            Toast.makeText(login.this, "Error!\n"+ task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
     }
 }
